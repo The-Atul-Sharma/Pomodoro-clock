264ad4c3c2584911f586d1015dc19eb2aea44acb
Atul Sharma <theatsharma@gmail.com>
2016-02-29 00:47:10 +0530

    change javascript of pomodoro clock

--- main.js
+++ main.js
@@ -59,6 +59,7 @@ app.controller('mainCtrl', function($scope, $interval){
         $scope.current = $scope.session;
         $scope.timer = $scope.session;
         $scope.pausebutton = " Pause";
+        $( "#pausebtn > i" ).removeClass( "fa fa-play" ).addClass( "fa fa-pause" );
       } else {
         $scope.current = $scope.break;
         $scope.timer = $scope.break;
@@ -66,10 +67,18 @@ app.controller('mainCtrl', function($scope, $interval){
       
       updateTime();
       click = $interval(updateTime, 1000);
+      if (!click) {
+        $scope.pausebutton = " Start";
+        $( "#pausebtn > i" ).removeClass( "fa fa-pause" ).addClass( "fa fa-play" );
+      }else{
+        $scope.pausebutton = " Pause";
+        $( "#pausebtn > i" ).removeClass( "fa fa-play" ).addClass( "fa fa-pause" );
+      }
     } else {
       $interval.cancel(click);
       click = false;
       $scope.pausebutton = " Start";
+      $( "#pausebtn > i" ).removeClass( "fa fa-pause" ).addClass( "fa fa-play" );
     }
   }
 
@@ -83,12 +92,16 @@ app.controller('mainCtrl', function($scope, $interval){
         $scope.timer = $scope.session * 60;
         $scope.initialTime = $scope.session;
         seconds = $scope.session * 60;
+        var audio = new Audio('http://www.freesfx.co.uk/rx2/mp3s/7/8478_1353333022.mp3');
+        audio.play();
       } else {
         $scope.status = 'Break';
         $scope.current = $scope.break;
         $scope.timer = $scope.break * 60;
         $scope.initialTime = $scope.break;
         seconds = $scope.break * 60;
+        var audio = new Audio('http://www.freesfx.co.uk/rx2/mp3s/7/8478_1353333022.mp3');
+        audio.play();
       }
     }else {
       $scope.timer = convertSeconds(seconds);
@@ -102,16 +115,12 @@ app.controller('mainCtrl', function($scope, $interval){
     $scope.break = 5;
     $scope.timer = 0;
     $scope.pausebutton = " Start";
-    if($scope.status === 'Session'){
-      $scope.current = $scope.session;
-      $scope.timer = $scope.session;
-      seconds = $scope.timer * 60;
-      
-      } else {
-        $scope.current = $scope.break;
-        $scope.timer = $scope.break;
-        seconds = $scope.timer * 60;
-      }
+    $( "#pausebtn > i" ).removeClass( "fa fa-pause" ).addClass( "fa fa-play" );
+    $scope.status = 'Session';
+    $scope.current = $scope.session;
+    $scope.timer = $scope.session;
+    seconds = $scope.timer * 60;
+    
   };
 
 });
